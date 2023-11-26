5ebe63ba9fcf00605ce8496661e7e9c7e49d251a
samual keleb <samualkelebu@gmail.com>
2023-11-25 20:37:43 -0800

    Thise is the BMI generator that i made by using vanila javascript in GDSC AASTU study session day 2 task

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+const Mass = +prompt("enter what you weigh! ");
+const Height = +prompt("enter your height!");
+const BMI = Mass / (Height * Height);
+console.log("your body mass index is" + "" + BMI);
